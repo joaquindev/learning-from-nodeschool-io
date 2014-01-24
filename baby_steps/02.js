0e50bebcac7ac4839eeccff22c314356f6c590bb
JoaquinDev <joaquin@isla.io>
2014-01-24 14:41:21 +0100

    Create example MY FIRST I/O! in Learnyounode
    Create example MY FIRST I/O! in Learnyounode
    
    Contact: joaquin@isla.io

--- baby_steps/02.js
+++ baby_steps/02.js
@@ -1,2 +1,5 @@
 var total = 0; 
-for(var i = 2; i<process.argv.length;i++){total += Number(process.argv[i]);} console.log(total);
+for(var i = 2; i<process.argv.length;i++){
+  total += Number(process.argv[i]);
+}
+console.log(total);
---
+++ my_first_i-o/03.js
@@ -0,0 +1,5 @@
+// MY FIRST I/O!
+/* Write a program that uses a single synchronous filesystem operation to read a file and print the number of newlines it contains to the console (stdout), similar to running cat file | wc -l*/
+
+
+
